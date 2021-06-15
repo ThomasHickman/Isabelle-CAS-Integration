5ffc3322d8f7493b4fabcac5962644ce936f9593
Simon Foster <simon.foster@york.ac.uk>
2021-06-15 15:33:00 +0100

    Began integration with shallow expression model

---
+++ Subst_ODE.thy
@@ -0,0 +1,10 @@
+section \<open> Substitutions as ODEs \<close>
+
+theory Subst_ODE
+  imports "Shallow-Expressions.Substitutions"
+begin
+
+ML_file \<open>Arith_Expr.ML\<close>
+ML_file \<open>Subst_ODE.ML\<close>
+
+end
