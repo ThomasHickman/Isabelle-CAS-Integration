35b5bd27ef4483adf88144f85450f6e82ce16979
Simon Foster <simon.foster@york.ac.uk>
2021-06-16 13:57:54 +0100

    Completed the Wolfram CAS integration modification

--- Subst_ODE.thy
+++ Subst_ODE.thy
@@ -11,32 +11,23 @@ ML_file \<open>wolfram-integration/parse_mathematica.ML\<close>
 ML_file \<open>wolfram-integration/isabelle_to_mathematica.ML\<close>
 ML_file \<open>wolfram-integration/mathematica_to_isabelle.ML\<close>
 
-term "[x \<leadsto> 1, y \<leadsto> $x + $y]"
-
-term "(\<notin>)"
-
-
 ML \<open> 
 
-  Syntax.check_term @{context} (Syntax.const "Set.not_member");
+structure Solve_Subst_ODE =
+struct
+  fun solve_subst_ode ctx sode =
+  let
+    open Isabelle_To_Mathematica; open Mathematica_To_Isabelle; open Subst_ODE;
+    val sode = Subst_ODE.subst_ode "t" sode;
+    val out = mathematica_output (translate_sode sode);
+    val mexp = Parse_Mathematica.parse out;
+    val rules = (map (map to_rule o to_list) o to_list) mexp;
+  in Syntax.check_term ctx (ode_subst ctx "t" (interpret_ode sode (hd rules)))
+  end;
 
-  open Isabelle_To_Mathematica; open Mathematica_To_Isabelle; open Subst_ODE;
-  val sode = Subst_ODE.subst_ode "t" @{term "[x \<leadsto> 1]"};
-  val out = mathematica_output (translate_sode "t" sode);
-(*
-      (translate_sode "t"
-        (Arith_Expr.sode_conv (mk_var_conv sode) sode));
-*)
-  val mexp = Parse_Mathematica.parse out;
-  val rules = (map (map to_rule o to_list) o to_list) mexp;
-  hd rules;
-  interpret_ode sode (hd rules)
-
-(*
-val e = aexp_sexp @{context} (sexp_aexp @{term "get\<^bsub>x\<^esub> s + 5 + y"});
-val e' = Syntax.check_term @{context} e;
-(Syntax.string_of_term @{context} e) |> Active.sendback_markup_command |> writeln;
-*)
+  fun solve_subst_ode_cmd ctx sode = 
+    "Found ODE solution: " ^ Active.sendback_markup_command (Syntax.string_of_term ctx (solve_subst_ode ctx sode)) |> writeln;
+end;
 \<close>
 
 end
