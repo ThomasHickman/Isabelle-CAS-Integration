584f75c9899edb624d747522c340e1a4ab3624b3
Simon Foster <simon.foster@york.ac.uk>
2021-06-16 09:48:00 +0100

    Changed the definition of an ODE so it specifies its independent variable, and almost finished adapting ODE parsing from Wolfram

--- Subst_ODE.thy
+++ Subst_ODE.thy
@@ -20,17 +20,23 @@ ML \<open>
 
   Syntax.check_term @{context} (Syntax.const "Set.not_member");
 
-  open Isabelle_To_Mathematica; open Subst_ODE;
-  val sode = Subst_ODE.subst_ode @{term "[x \<leadsto> 1, y \<leadsto> $x]"};
-  val out = mathematica_output
-      (translate_sode "t" 
+  open Isabelle_To_Mathematica; open Mathematica_To_Isabelle; open Subst_ODE;
+  val sode = Subst_ODE.subst_ode "t" @{term "[x \<leadsto> 1]"};
+  val out = mathematica_output (translate_sode "t" sode);
+(*
+      (translate_sode "t"
         (Arith_Expr.sode_conv (mk_var_conv sode) sode));
-  Parse_Mathematica.parse out;
+*)
+  val mexp = Parse_Mathematica.parse out;
+  val rules = (map (map to_rule o to_list) o to_list) mexp;
+  hd rules;
+  interpret_ode sode (hd rules)
 
+(*
 val e = aexp_sexp @{context} (sexp_aexp @{term "get\<^bsub>x\<^esub> s + 5 + y"});
 val e' = Syntax.check_term @{context} e;
 (Syntax.string_of_term @{context} e) |> Active.sendback_markup_command |> writeln;
-
+*)
 \<close>
 
 end
